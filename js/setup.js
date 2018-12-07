0568855256e6907f90a27adc300b6947de230b3b
Saire28 <huntergeass@gmail.com>
2018-12-07 18:01:32 +0300

    Выполнил 4 домашнее задание в учебном проекте
    Открытие и закрытие окна по нажатию, валидация, изменение цвета глаз, мантии и фаерболла

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,128 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var dialogMenu = document.querySelector('.setup');
-dialogMenu.classList.remove('hidden');
+var inputName = dialogMenu.querySelector('.setup-user-name');
+var dialogMenuOpen = document.querySelector('.setup-open');
+var dialogMenuClose = dialogMenu.querySelector('.setup-close');
+
+var coatFillList = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesFillList = ['black', 'red', 'blue', 'yellow', 'green'];
+var fireballFillList = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+// функция возврата случайного элемента массива
+
+var getRandomArrayElement = function (array) {
+  return array[Math.round(Math.random() * (array.length - 1))];
+};
+
+// функция возврата события evt со значением кнопки ESC
+
+var isEscEvent = function (evt) {
+  return evt.keyCode === ESC_KEYCODE;
+};
+
+// описываем открытие и закрытие диалогового окна персонажа по нажатию на кнопки мыши и клавиатурой
+
+var onDialogMenuEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeDialogMenu();
+  }
+};
+
+var openDialogMenu = function () {
+  dialogMenu.classList.remove('hidden');
+  document.addEventListener('keydown', onDialogMenuEscPress);
+};
+
+var closeDialogMenu = function () {
+  dialogMenu.classList.add('hidden');
+};
+
+// открытие окна при щелчке мышью на иконке
+
+dialogMenuOpen.addEventListener('click', function () {
+  openDialogMenu();
+});
+
+// открытие окна при нажатии ENTER
+
+dialogMenuOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openDialogMenu();
+  }
+});
+
+// закрытие окна при нажатии на крестик
+
+dialogMenuClose.addEventListener('click', function () {
+  closeDialogMenu();
+});
+
+// закрытие окна при нажатии ENTER на крестике с фокусом
+
+dialogMenuClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeDialogMenu();
+  }
+});
+
+// отмена действия кнопки ESC при фокусе на input
+
+inputName.addEventListener('keydown', function (evt) {
+  if (isEscEvent(evt)) {
+    evt.stopPropagation();
+  }
+});
+
+// расширяем валидацию полей формы. Изменяем появляющееся сообщение
+
+var userNameInput = dialogMenu.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+// поиск SVG элемента с мантией и глазами мага
+
+var setupWizard = document.querySelector('.setup-wizard');
+var wizardCoatField = setupWizard.querySelector('.wizard-coat');
+var wizardEyesField = setupWizard.querySelector('.wizard-eyes');
+
+// изменение цвета мантии мага
+
+wizardCoatField.addEventListener('click', function () {
+  wizardCoatField.style.fill = getRandomArrayElement(coatFillList);
+  document.querySelector('input[name=coat-color]').value = wizardCoatField.style.fill;
+});
+
+// изменение цвета глаз мага
+
+wizardEyesField.addEventListener('click', function () {
+  wizardEyesField.style.fill = getRandomArrayElement(eyesFillList);
+  document.querySelector('input[name=eyes-color]').value = wizardEyesField.style.fill;
+});
+
+// изменение цвета фаерболла
+
+var fireballField = document.querySelector('.setup-fireball-wrap');
+
+fireballField.addEventListener('click', function () {
+  fireballField.style.background = getRandomArrayElement(fireballFillList);
+  document.querySelector('input[name=fireball-color]').value = fireballField.style.background;
+});
+
+// поиск шаблона мага
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -17,28 +138,22 @@ var wizardCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100,
 
 var wizardEyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var wizardList = [
-  {
-    name: randomizeData(wizardName) + ' ' + randomizeData(wizardSurname),
-    coatColor: randomizeData(wizardCoatColor),
-    eyesColor: randomizeData(wizardEyesColor)
-  },
-  {
-    name: randomizeData(wizardName) + ' ' + randomizeData(wizardSurname),
-    coatColor: randomizeData(wizardCoatColor),
-    eyesColor: randomizeData(wizardEyesColor)
-  },
-  {
-    name: randomizeData(wizardName) + ' ' + randomizeData(wizardSurname),
-    coatColor: randomizeData(wizardCoatColor),
-    eyesColor: randomizeData(wizardEyesColor)
-  },
-  {
-    name: randomizeData(wizardName) + ' ' + randomizeData(wizardSurname),
-    coatColor: randomizeData(wizardCoatColor),
-    eyesColor: randomizeData(wizardEyesColor)
+// функция создания массива с объектами магов
+
+var getWizardList = function () {
+  var wizards = [];
+
+  for (var i = 0; i < 4; i++) {
+    wizards[i] = {
+      name: randomizeData(wizardName) + ' ' + randomizeData(wizardSurname),
+      coatColor: randomizeData(wizardCoatColor),
+      eyesColor: randomizeData(wizardEyesColor)
+    };
   }
-];
+  return wizards;
+};
+
+var wizardList = getWizardList();
 
 var fragment = document.createDocumentFragment();
 
